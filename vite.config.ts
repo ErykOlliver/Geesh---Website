9e1a7e16342a5fdd7d79cca0ad3b531fab77b8bf
Eryk Olliver <erykolliver@gmail.com>
2025-08-25 09:28:56 -0300

    endereço da pagina alterado

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Geesh---Website',
+  base: '/GeeshWebsite',
   plugins: [
     react(),
     tailwindcss(),
